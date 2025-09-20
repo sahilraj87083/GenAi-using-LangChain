cd188363b84d3f812e1fd36d6a41b1767b3720d1
sahilraj87083 <sahilraj87093@gmail.com>
2025-09-20 22:59:23 +0530

    Dummy commit

--- 1.LLMs/1_llm_demo.py
+++ 1.LLMs/1_llm_demo.py
@@ -1,11 +1,11 @@
-from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAI
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo-instruct")
+llm = OpenAI(model_name="gpt-3.5-turbo-instruct")
 
 result = llm.invoke("What is the capital of India?")
 print(result)
